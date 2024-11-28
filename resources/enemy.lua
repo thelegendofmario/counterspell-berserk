6d4cbc76d971032d5f6ae4fcff4a0f4af3838305
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-11-28 12:31:06 -0500

    feat: adjust enemy rebound chance to limit maximum value

--- resources/enemy.lua
+++ resources/enemy.lua
@@ -3,7 +3,7 @@ enemy.tile_x = 6
 enemy.tile_y = 4
 enemy.image = love.graphics.newImage('resources/sprites/enemy.png')
 enemy.speed = 1
-enemy.rebound_chance = math.max(0.15, math.random())
+enemy.rebound_chance = math.min(math.max(0.15, math.random()), 0.3)
 
 function enemy:update(player, swords)
     local x = self.tile_x
