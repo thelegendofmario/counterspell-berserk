42ae924c59ba5c1cb1f7a3d2c1cba75264274de8
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-11-28 18:54:42 -0500

    chore: update package script to use nix-shell for zipping files

--- package.sh
+++ package.sh
@@ -1,8 +1,5 @@
-# Define the output zip file
-output_zip="berserk.love"
-
 # Find and zip the files
-find . -type f \( -name "*.lua" -o -name "*.png" -o -name "*.mp3" \) -print | zip -@ "$output_zip"
+nix-shell -p zip --run 'find . -type f \( -name "*.lua" -o -name "*.png" -o -name "*.mp3" \) -print | zip -@ berserk.love'
 
 bunx love.js berserk.love berserk -c
 
