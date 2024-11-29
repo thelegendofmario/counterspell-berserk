bc593ec245ca35a34f1f9f2b60c34c5f986201f5
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-11-28 21:19:56 -0500

    feat: add hurt sound and make the swords go further

--- main.lua
+++ main.lua
@@ -150,7 +150,7 @@ function love.keypressed(k)
                             x = dir.x * 4 + dir.x * 4 * speedAdjustment,
                             y = dir.y * 4 + dir.y * 4 * speedAdjustment
                         },
-                        timeOut = 2.5,
+                        timeOut = 3.5,
                         frame = 1,
                         name = "sword" .. game.totalSpawnedEnemy
                     }
@@ -210,6 +210,7 @@ function love.update(dt)
             for _, enemy in ipairs(enemies) do
                 if player.tile_x == enemy.tile_x and player.tile_y == enemy.tile_y then
                     player.hearts = player.hearts - enemy.damage
+                    TEsound.play("resources/sfx/hurt1.mp3", "static", "sfx", 0.3)
                 end
             end
             for i, sword in ipairs(swords) do
@@ -237,6 +238,7 @@ function love.update(dt)
                         quad.twidth + 3 and sword.y >= player.tile_y * quad.theight - quad.theight - 3 and sword.y <=
                         player.tile_y * quad.theight + 3 then
                         player.hearts = player.hearts - 1
+                        TEsound.play("resources/sfx/hurt1.mp3", "static", "sfx", 0.3)
                         sword.x = sword.x + 4
                         table.remove(swords, i)
                         player.swords = player.swords + 1
