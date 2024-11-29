14d1cdd06618c7410f6f2c819ee1b9485320b5b9
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-11-28 19:20:09 -0500

    bug: fix script

--- package.sh
+++ package.sh
@@ -3,6 +3,8 @@ nix-shell -p zip --run 'find . -type f \( -name "*.lua" -o -name "*.png" -o -nam
 
 bunx love.js berserk.love berserk -c
 
-scp -r berserk hackclub.app:/home/kierank/berserk
+scp -r berserk/* hackclub.app:/home/kierank/berserk/
+
+ssh nest.hackclub.app systemctl caddy restart
 
 rm -rf berserk.love berserk
